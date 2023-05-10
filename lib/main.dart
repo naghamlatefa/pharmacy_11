5a23f0958e06eae91e30985b2a9c1483d29661cf
naghamlatefa <naghamlatefa2@gmail.com>
2023-05-10 20:47:12 +0300

    nnn

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 }
 class MyApp extends StatelessWidget{
   @override
-  Widget  build(BuildContext context){
+  Widget   build(BuildContext context){
     return MaterialApp(
         debugShowCheckedModeBanner: false ,
         home:login());
