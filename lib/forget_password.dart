072864c6f71b6c01abb3a27021e3555984c67f25
naghamlatefa <naghamlatefa2@gmail.com>
2023-08-09 11:20:02 +0300

    e

--- lib/forget_password.dart
+++ lib/forget_password.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'new_password.dart';
+import 'package:pharmacy_1/main.dart';
 import 'forget_password2.dart';
+import 'main.dart';
+import 'package:http/http.dart' as http;
 class forget_password extends StatefulWidget {
   const forget_password({Key ? key}) : super(key: key);
 
@@ -10,6 +13,43 @@ class forget_password extends StatefulWidget {
 }
 
 class _forget_passwordState extends State<forget_password> {
+  Future email(String email)async{
+    print("111");
+
+    print("enaillllll$email");
+    var response = await http.post(
+        Uri.parse('$url/api/user/password/email'),
+        body:<String,String>
+        {
+          'email':email,
+        },
+        headers: {"Accept":"application/json",
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $Token'
+
+        }
+    );
+    print("response is ${response.body}");
+    print("response is ${response.statusCode}");
+    if(response.statusCode==201)
+    {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('the code sent to your Email')));
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => forget_password2(),
+        ),
+      );
+      var js=jsonDecode(response.body);
+      Token=js['token'];
+
+    }
+    else
+    {
+      print("sorry");
+    }
+  }
+  var emaill = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +64,7 @@ class _forget_passwordState extends State<forget_password> {
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: TextFormField(
+                controller: emaill,
                 cursorColor: Color.fromRGBO(13,142,171, 1),
                 decoration: InputDecoration(
                     focusedBorder: OutlineInputBorder(
@@ -52,12 +93,8 @@ class _forget_passwordState extends State<forget_password> {
                 child: MaterialButton(
                   height: 50,
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => forget_password2(),
-                      ),
-                    );
+                    email(emaill.text);
+
                   },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
